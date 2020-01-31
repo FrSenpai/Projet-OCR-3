aa54e71ffd06c93173af44a577cff9321dd59540
FrSenpai <Collosh@hotmail.fr>
2020-01-31 09:20:01 +0100

    Mise en place du repo github

---
+++ js/reservation.js
@@ -0,0 +1,113 @@
+class Reservation {
+
+    // Variables DOM
+    prenomElt = document.getElementById("prenom");
+    nomElt = document.getElementById("nom");
+    boutonPanelReservationElt = document.getElementById("boutonReserver");
+    panelReservationElt = document.getElementById("panelReservation");
+    panelCanvasElt = document.getElementById("panelCanvas");
+    boutonPanelCanvasElt = document.getElementById("validationCanvas");
+    infoReservationElt = document.getElementById("reservationEnCours");
+    tempsRestantElt = document.getElementById("tempsRestant");
+    annulerReservationElt = document.getElementById("annulerReservation"); // Annulation via l'onglet du temps restant
+
+    // Autres
+    initTimer = new Timer();
+    prenomUtilisateur = "";
+    nomUtilisateur = "";
+
+    constructor() {
+        this.initEvent();
+        
+    }
+
+    initEvent() {
+        // Afficher la div uniquement quand chargement ok
+        window.addEventListener("load", (e) => {
+            if (sessionStorage.getItem("minutes") && sessionStorage.getItem("secondes")) { // Si une réservation est déjà en cours, relance le timer.
+                console.log("Reservation en cache !")
+                timer.etatTimer = true;
+                timer.timer();
+            } else {
+                timer.etatTimer = false;
+            }
+        })
+        
+        // Gestion des interactions de l'utilisateur.
+        this.prenomElt.addEventListener("click", (e) => this.informationsConnu());
+
+        this.boutonPanelReservationElt.addEventListener("click", (e) => {
+            this.verifierInformations();
+            // Si input prénom & nom sont corrects, alors envoyer les données, sinon "" "" ""
+            if (this.prenomOk === 1 && this.nomOk === 1) {
+                localStorage.setItem("prenom", this.prenomUtilisateur);
+                localStorage.setItem("nom", this.nomUtilisateur);
+                this.panelReservationElt.style.display = "None";
+                this.panelCanvasElt.style.display = "flex";
+            } else {
+                // Afficher un message d'informations
+            }
+        })
+
+        this.boutonPanelCanvasElt.addEventListener("click", (e) => {
+            if (canvas.signatureOK === true) {
+                timer.etatTimer = true;
+                timer.timer();
+                document.getElementById("map").style.width = "90%";
+                this.panelCanvasElt.style.display = "none";
+                document.getElementById("signatureInvalide").style.visibility = "hidden";
+                canvas.signatureOK = false;
+                console.log(timer.minutes, timer.secondes);
+                this.tempsRestantElt.textContent = `Votre reservation expire dans 20 minutes !`;
+            } else {
+                document.getElementById("signatureInvalide").style.visibility = "visible";
+            }
+
+        })
+            // annuler une réservation
+        this.annulerReservationElt.addEventListener("click", (e) => {
+            timer.stopTimer();
+        })
+    }
+    
+    
+    
+    informationsConnu() {
+        // Si un prénom ou un nom ont été déjà envoyé, les inputs les prennent comme valeur
+        if (localStorage.getItem("nom")) {
+            this.nomElt.value = localStorage.getItem("nom");
+        }
+        if (localStorage.getItem("prenom")) {
+            this.prenomElt.value = localStorage.getItem("prenom");
+        }
+    }
+
+    verifierInformations() {
+        const regex = /[a-z]/;
+        // Limite de caractères
+
+        // Si le prénom est syntaxiquement correct alors
+        if (!regex.test(this.prenomElt.value)) {
+            console.log("Prénom PAS ok !");
+            this.prenomOk = 0;
+            document.getElementById("prenomRequis").style.visibility = "visible";
+        } else { // To-do : Affichage d'un message de prévention en cas d'erreur dans le prénom
+            console.log("Prénom ok !");
+            this.prenomUtilisateur = this.prenomElt.value;
+            this.prenomOk = 1;
+            document.getElementById("prenomRequis").style.visibility = "hidden";
+        }
+        // Si le nom est syntaxiquement correct alors
+        if (!regex.test(this.nomElt.value)) {
+            console.log("Nom PAS ok !");
+            this.nomOk = 0 // Nom incorrect !
+            document.getElementById("nomRequis").style.visibility = "visible";
+        } else {
+            console.log("Nom Ok !");
+            this.nomUtilisateur = this.nomElt.value;
+            this.nomOk = 1; // Nom ok !
+            document.getElementById("nomRequis").style.visibility = "hidden";
+        }
+    }
+
+}
---
+++ js/slider.js
@@ -0,0 +1,112 @@
+class Slider {
+    timer = 5; // Le timer en s
+    imgActuelle = 0; // L'image actuelle affichée sur le slider, par défault = 0
+    etatSlider = 1; // 1 = En route, 0 = En pause
+
+    // TO DO : Rajouter Elt sur les variables qui concernent des élements du DOM
+
+    constructor() {
+        this.flecheGauche = document.getElementById("flecheGauche");
+        this.flecheDroite = document.getElementById("flecheDroite");
+        this.image = document.getElementById("imgSlider");
+        this.descriptionSliderElt = document.getElementById("descriptionSlider");
+        this.navigateur = window;
+        this.buttonPause = document.getElementById("pause-Play");
+        this.timerImage();
+
+
+        this.flecheGauche.addEventListener("click", (e) => this.imagePrecedente());
+        this.flecheDroite.addEventListener("click", (e) => this.imageSuivante());
+        this.buttonPause.addEventListener("click", (e) => this.pauseReprise());
+        this.navigateur.addEventListener("keydown", (e) => this.gestionTouche());
+
+    }
+
+    imagePrecedente() {
+        // Se produit lors d'un clic sur la flèche gauche présente sur le slider
+
+        if (this.imgActuelle <= -1) {
+            this.imgActuelle = 1;
+        } else {
+            this.imgActuelle--;
+        }
+        this.image.src = `../media/sliderbackground-${this.imgActuelle}.jpg`;
+        this.timer = 5;
+        if (this.imgActuelle === 0) {
+            this.descriptionSliderElt.textContent = `Première étape : Cliquez sur la station de votre choix !`;
+        } else if (this.imgActuelle === 1) {
+            this.descriptionSliderElt.textContent = `Deuxième étape : Remplissez le formulaire de réservation`;
+        } else if (this.imgActuelle === -1) {
+            this.descriptionSliderElt.textContent = `Dernière étape : Signez et finalisez votre réservation !`;
+        }
+
+    }
+
+    imageSuivante() {
+        // Se produit lors d'un clic sur la flèche droite présente sur le slider
+
+
+        if (this.imgActuelle >= 1) {
+            this.imgActuelle = -1;
+        } else {
+            this.imgActuelle++;
+        }
+
+        this.image.src = `../media/sliderbackground-${this.imgActuelle}.jpg`;
+        this.timer = 5;
+
+        if (this.imgActuelle === 0) {
+            this.descriptionSliderElt.textContent = `Première étape : Cliquez sur la station de votre choix !`;
+        } else if (this.imgActuelle === 1) {
+            this.descriptionSliderElt.textContent = `Deuxième étape : Remplissez le formulaire de réservation`;
+        } else if (this.imgActuelle === -1) {
+            this.descriptionSliderElt.textContent = `Dernière étape : Signez et finalisez votre réservation !`;
+        }
+
+    }
+
+    timerImage() {
+        // Change de slide toutes les 5 secondes, si interaction de l'utilisateur, reset le timer (imageSuivante,imagePrecedente).
+
+
+        setInterval(() => {
+            // Si le timer est en route, le décrementer, sinon, ne rien faire
+            if (this.etatSlider > 0) {
+                this.timer--;
+            } else {}
+
+
+            if (this.timer === 0) {
+                this.imageSuivante();
+            } else {}
+        }, 1000);
+
+
+
+    }
+
+    gestionTouche() {
+        // Gère les appuis de touches 
+        if (event.keyCode === 37) { // flèche gauche
+            return this.imagePrecedente();
+        } else if (event.keyCode === 39) { // flèche droite
+            return this.imageSuivante();
+        } else {
+            console.log("Touche non définie");
+        }
+    }
+
+    pauseReprise() {
+        console.log("Click !")
+        // Si le timer est en route, mise en pause lors du clic, sinon remise en route !
+        if (this.etatSlider > 0) {
+            this.etatSlider = 0;
+            this.buttonPause.className = "far fa-play-circle";
+        } else {
+            this.etatSlider = 1;
+            this.buttonPause.className = "far fa-pause-circle";
+        }
+    }
+
+
+}
